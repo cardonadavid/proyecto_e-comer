a45f40c69726280180ee82d70de0fec2949a3d1d
cardonadavid <cardonagaleanocristiandavid@gmail.com>
2024-08-05 15:49:05 -0500

    paguina principal del e-comer

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tienda</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="logo">
+            <img src="assets/img/home.svg">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#productos">Productos</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+                <li><a href="#carrito">Carrito de Compras</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Sección de oferta -->
+
+    <div class="jumbotron">
+        <span>
+            <h2>Oferta especial</h2>
+        </span>
+
+    </div>
+
+    <!-- Sección de Productos Destacados -->
+    <h2 style="text-align: center;">Productos destacados</h2>
+    <div class="container">
+        <div class="card">
+            <img src="producto1.jpg" alt="Producto 1">
+            <h3>Producto 1</h3>
+            <p class="price">$20.00</p>
+            <p>Descripción breve del producto 1.</p>
+        </div>
+        <div class="card">
+            <img src="producto2.jpg" alt="Producto 2">
+            <h3>Producto 2</h3>
+            <p class="price">$30.00</p>
+            <p>Descripción breve del producto 2.</p>
+        </div>
+        <div class="card">
+            <img src="producto3.jpg" alt="Producto 3">
+            <h3>Producto 3</h3>
+            <p class="price">$40.00</p>
+            <p>Descripción breve del producto 3.</p>
+        </div>
+        <div class="card">
+            <img src="producto4.jpg" alt="Producto 4">
+            <h3>Producto 4</h3>
+            <p class="price">$50.00</p>
+            <p>Descripción breve del producto 4.</p>
+        </div>
+    </div>
+    <!-- Pie de Página -->
+    <footer>
+        <p>Contacto: contacto@tienda.com</p>
+        <div class="redes-sociales">
+            <a href="#">Facebook</a>
+            <a href="#">Twitter</a>
+            <a href="#">Instagram</a>
+        </div>
+        <p><a href="#">Políticas de Privacidad</a></p>
+    </footer>
+</body>
+
+</html>
